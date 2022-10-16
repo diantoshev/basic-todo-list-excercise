a590f6fe0a32f529baa4535afb121eef5c40d1af
Dian Toshev <dian.toshev@gmail.com>
2022-10-16 20:41:15 +0300

    fix commit error

--- src/components/TODO-LIST/ToDoItem.js
+++ src/components/TODO-LIST/ToDoItem.js
@@ -1,26 +1,16 @@
 import Card from '../UI/Card';
 import './ToDoItem.css';
-import React, { useState } from 'react';
 
 export default function ToDoItem(props) {
 
     const date = new Date();
 
-    let [strikeClass, setstrikeClass] = useState(true);
-
-    const classClickHandler = () => {
-        setstrikeClass(strikeClass => !strikeClass);
-    }
-
-    let toggleClass = strikeClass ? 'no-strike' : 'strike';
-
-
     return (
-        <Card className='todo-item' value={props.id}>
+        <Card className='todo-item'>
             <div className='date'> {date.toDateString()}</div>
             <div className='todo-item__description'>
-                <h2 onClick={classClickHandler} className={toggleClass}>{props.textToDo}</h2>
-                <button onClick={() => props.onDelete(props.id)} className='todo-item__btn'>Remove</button>
+                <h2>{props.textToDo}</h2>
+                <button className='todo-item__btn'>Remove</button>
             </div>
         </Card>
     )
--- src/components/TODO-LIST/ToDoList.js
+++ src/components/TODO-LIST/ToDoList.js
@@ -2,27 +2,17 @@ import './ToDoList.css';
 import ToDoItem from './ToDoItem';
 import Card from '../UI/Card';
 import '../UI/Card.css';
+import NewToDo from '../ADD-TODO/NewToDo';
 
 export default function ToDoList(props) {
-
-    const deleteHandler = (id) => {
-        props.onDeleteItem(id);
-    }
     return (
         <Card className='toDoList_container'>
-            <div>Your To-Do items will apear here:</div>
-            {props.tasks.map((task) => {
-                return < div >
-                    <ToDoItem
-                        key={task.id}
-                        textToDo={task.value}
-                        id={task.id}
-                        tasks={props.tasks}
-                        onDelete={deleteHandler} />
-                </div>
-            })}
-            {localStorage.setItem("toDos", JSON.stringify(props.tasks))}
-        </Card >
+            <NewToDo />
+            <div><ToDoItem textToDo={props.tasks[0]} /></div>
+            <div><ToDoItem textToDo={props.tasks[1]} /></div>
+            <div><ToDoItem textToDo={props.tasks[2]} /></div>
+            <div><ToDoItem textToDo={props.tasks[3]} /></div>
+        </Card>
     )
 };
 
